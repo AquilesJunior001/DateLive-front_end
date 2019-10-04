6d85734f92e83a336d5fa417d619660f84efe678
AquilesJunior001 <aquilesjunior9@gmail.com>
2019-10-04 15:42:57 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicialComponent } from './inicial/inicial.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { ExemploComponent } from './exemplo/exemplo.component';
+import { ExercicioSemPontuacaoComponent } from './exercicio-sem-pontuacao/exercicio-sem-pontuacao.component';
+import { ExercicioComPontuacaoComponent } from './exercicio-com-pontuacao/exercicio-com-pontuacao.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 
 const routes: Routes = [
-  {path:'', component: LoginComponent}
+  {path:'', component: LoginComponent},
+  {path: 'cadastrar', component: CadastroComponent},
+  {path: 'inicial', component: InicialComponent},
+  {path: 'conteudos', component: ConteudoComponent},
+  {path: 'exemplos', component: ExemploComponent},
+  {path: 'exercicios', component: ExercicioSemPontuacaoComponent},
+  {path: 'exerciciosP', component: ExercicioComPontuacaoComponent},
+  {path: 'usuario', component: UsuarioComponent}
 ];
 
 @NgModule({
--- src/app/funcoes.service.ts
+++ src/app/funcoes.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class FuncoesService {
 
   constructor(private http: HttpClient) { }
-  URL = 'http://127.0.0.1:5000/cadastro';
+  URL = 'http://127.0.0.1:5000/select';
 
   lista() {
     return this.http.get(this.URL)
